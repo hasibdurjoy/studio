9ea06dc58ca640cfd1c959641f723d38e8b2e1db
hasibdurjoy <hasibdurjoy5@gmail.com>
2023-08-27 01:10:22 +0600

    hook done

--- src/Components/HookProject/HookProjectHome.jsx
+++ src/Components/HookProject/HookProjectHome.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import HookBanner from "./HookBanner";
+import MiddlePart from "./MiddlePart";
+import HookFeatures from "./HookFeatures";
+import Hook from "./Hook";
 
 const HookProjectHome = () => {
   return (
     <div>
       <HookBanner />
+      <MiddlePart />
+      <HookFeatures />
+      <Hook />
     </div>
   );
 };
