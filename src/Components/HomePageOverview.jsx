3090774fed7e56a98bd9e641b3b874b6d00ef998
hasibdurjoy <hasibdurjoy5@gmail.com>
2023-08-27 10:30:08 +0600

    update main

---
+++ src/Components/HomePageOverview.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePageOverview = () => {
+  return (
+    <div>
+      <h2>Hi from overview</h2>
+    </div>
+  );
+};
+
+export default HomePageOverview;
---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomePageOverview from "../Components/HomePageOverview";
+
+const HomePage = () => {
+  return (
+    <div>
+      <HomePageOverview />
+    </div>
+  );
+};
+
+export default HomePage;
