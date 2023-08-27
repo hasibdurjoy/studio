0a32c62981798aea47e95dfc273f31b6ccbae21c
arindom10 <karindom7@gmail.com>
2023-08-28 00:49:26 +0600

    WhatWeDo icon changed

--- src/Component/WhatWeDo.jsx
+++ src/Component/WhatWeDo.jsx
@@ -4,64 +4,106 @@ import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { PiTelevisionSimpleBold } from "react-icons/pi";
+import { BiCubeAlt } from "react-icons/bi";
+import { SiWebauthn } from "react-icons/si";
+import { MdOutlineProductionQuantityLimits } from "react-icons/md";
+import { AiOutlineCloud } from "react-icons/ai";
+import { MdTouchApp } from "react-icons/md";
 
 const WhatWeDo = () => {
-  const fakeData = [
-    {
-      id: 1,
-      name: "Brand Identity",
-      icon: "<PiTelevisionSimpleBold />",
-    },
-    {
-      id: 2,
-      name: "Data Analytics",
-      icon: "<PiTelevisionSimpleBold />",
-    },
-    {
-      id: 3,
-      name: "Web Design",
-      icon: "<PiTelevisionSimpleBold />",
-    },
-    {
-      id: 4,
-      name: "Product Strategy",
-      icon: "<PiTelevisionSimpleBold />",
-    },
-    {
-      id: 5,
-      name: "Cloud Services",
-      icon: "<PiTelevisionSimpleBold />",
-    },
-    {
-      id: 6,
-      name: "Mobile Apps Development",
-    },
-  ];
-  return (
-    <div style={{ backgroundColor: "#F1F1F1" }}>
-      <Container style={{ padding: "100px 40px" }}>
-        <p style={{ color: "#cc147f" }}>/What We Do</p>
-        <h1 style={{ fontSize: "48px", padding: "15px 0px" }}>
-          We have everything you need to launch and grow a successful digital
-          business.
-        </h1>
-        <Row>
-          {fakeData.map((s) => (
-            <Col sm={12} md={6}>
-              <Card className="border-0 h-100 bg-transparent">
-                <Card.Body>
-                  <div className="fs-1" style={{ color: "#26BFB5" }}>
-                    <PiTelevisionSimpleBold />
-                  </div>
-                  <h4 className="mb-0 ">{s.name}</h4>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    </div>
-  );
+    return (
+        <div style={{ backgroundColor: "#F1F1F1" }}>
+            <Container style={{ padding: "100px 40px" }}>
+                <p style={{ color: "#cc147f" }}>/What We Do</p>
+                <h1 style={{ fontSize: "48px", padding: "15px 0px" }}>
+                    We have everything you need to launch and grow a successful
+                    digital business.
+                </h1>
+                <Row>
+                    <Col sm={12} md={6}>
+                        <Card className="border-0 h-100 bg-transparent">
+                            <Card.Body>
+                                <div
+                                    className="fs-1"
+                                    style={{ color: "#26BFB5" }}
+                                >
+                                    <PiTelevisionSimpleBold />
+                                </div>
+                                <h4 className="mb-0 ">Brand Identity</h4>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm={12} md={6}>
+                        <Card className="border-0 h-100 bg-transparent">
+                            <Card.Body>
+                                <div
+                                    className="fs-1"
+                                    style={{ color: "#26BFB5" }}
+                                >
+                                    <BiCubeAlt />
+                                </div>
+                                <h4 className="mb-0 ">Data Analytics</h4>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm={12} md={6}>
+                        <Card className="border-0 h-100 bg-transparent">
+                            <Card.Body>
+                                <div
+                                    className="fs-1"
+                                    style={{ color: "#26BFB5" }}
+                                >
+                                    <SiWebauthn />
+                                </div>
+                                <h4 className="mb-0 ">Web Design</h4>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm={12} md={6}>
+                        <Card className="border-0 h-100 bg-transparent">
+                            <Card.Body>
+                                <div
+                                    className="fs-1"
+                                    style={{ color: "#26BFB5" }}
+                                >
+                                    <MdOutlineProductionQuantityLimits />
+                                </div>
+                                <h4 className="mb-0 ">Product Strategy</h4>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm={12} md={6}>
+                        <Card className="border-0 h-100 bg-transparent">
+                            <Card.Body>
+                                <div
+                                    className="fs-1"
+                                    style={{ color: "#26BFB5" }}
+                                >
+                                    <AiOutlineCloud />
+                                </div>
+                                <h4 className="mb-0 ">Cloud Services</h4>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm={12} md={6}>
+                        <Card className="border-0 h-100 bg-transparent">
+                            <Card.Body>
+                                <div
+                                    className="fs-1"
+                                    style={{ color: "#26BFB5" }}
+                                >
+                                    <MdTouchApp />
+                                </div>
+                                <h4 className="mb-0 ">
+                                    Mobile Apps Developments
+                                </h4>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 };
 
 export default WhatWeDo;
