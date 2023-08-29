21db679536a6462c9d518bdfad4c6a298b050c01
hasibdurjoy <hasibdurjoy5@gmail.com>
2023-08-29 22:28:55 +0600

    header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="mx-5 pt-1">
+      <div className="d-flex align-items-center justify-content-between">
+        <div>
+          <img
+            src="https://www.mirumindia.com/hs-fs/hubfs/raw_assets/public/Mirum_June2022/images/mirum_logo-1.png?width=207&name=mirum_logo-1.png"
+            alt=""
+            height="60px"
+          />
+        </div>
+        <div className="d-flex align-items-center  gap-5">
+          <h6>Name 1</h6>
+          <h6>Name 1</h6>
+          <h6>Name 1</h6>
+          <h6>Name 1</h6>
+          <h6>Name 1</h6>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
