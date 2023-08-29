a3192e2f6eaf5308f1c56412dc8e2cd3f9ba7a8e
hasibdurjoy <hasibdurjoy5@gmail.com>
2023-08-29 21:48:32 +0600

    bg

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -5,64 +5,59 @@ import Navbar from "react-bootstrap/Navbar";
 
 function ColorSchemesExample() {
     return (
-        <>
-            <Navbar bg="light" data-bs-theme="light">
-                <Container>
-                    <Col md={6}>
-                        <img
-                            height="60px"
-                            src="https://i.ibb.co/JkNFQKC/1.png"
-                            alt=""
-                        />
-                    </Col>
+      <>
+        <Navbar bg="light" data-bs-theme="light">
+          <Container>
+            <Col md={6}>
+              <img height="60px" src="https://i.ibb.co/JkNFQKC/1.png" alt="" />
+            </Col>
 
-                    <Col md={6}>
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#aboutus">About Us</Nav.Link>
-                            <Nav.Link href="#service">Service</Nav.Link>
-                            <Nav.Link href="#blog">Blog</Nav.Link>
-                            <Nav.Link href="#contactus">Contact Us</Nav.Link>
-                        </Nav>
-                    </Col>
-                </Container>
-            </Navbar>
-            <div className="banner">
-                <Container>
-                    <Row>
-                        <Col md={6} className="banner-text mt-5">
-                            <br />
-                            <br />
-                            <br />
-                            <h4>We are Creative & Fast</h4>
-                            <br />
-                            <br />
-                            <h1>
-                                <strong>LEVEL UP</strong>
-                            </h1>
-                            <h1 className="text-danger ">
-                                <strong>YOUR BUSINESS</strong>
-                            </h1>
-                            <br />
-                            <br />
-                            <h5>
-                                Welcome to our professional, artistic and
-                                technical world!
-                            </h5>
-                            <br />
-                            <h5>EXPLORE PORTFOLIO</h5>
-                        </Col>
-                        <Col md={6} className="banner-image">
-                            <img
-                                className="h-75"
-                                src="https://creavast.com/wp-content/uploads/2021/01/hxZtEnhSdf-1.png"
-                                alt="Banner"
-                            />
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        </>
+            <Col md={6}>
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#aboutus">About Us</Nav.Link>
+                <Nav.Link href="#service">Service</Nav.Link>
+                <Nav.Link href="#blog">Blog</Nav.Link>
+                <Nav.Link href="#contactus">Contact Us</Nav.Link>
+              </Nav>
+            </Col>
+          </Container>
+        </Navbar>
+        <div className="banner">
+          <Container className="mx-4">
+            <Row>
+              <Col md={6} className="banner-text mt-5">
+                <br />
+                <br />
+                <br />
+                <h4>We are Creative & Fast</h4>
+                <br />
+                <br />
+                <h1>
+                  <strong>LEVEL UP</strong>
+                </h1>
+                <h1 className="text-danger ">
+                  <strong>YOUR BUSINESS</strong>
+                </h1>
+                <br />
+                <br />
+                <h5>
+                  Welcome to our professional, artistic and technical world!
+                </h5>
+                <br />
+                <h5>EXPLORE PORTFOLIO</h5>
+              </Col>
+              <Col md={6} className="banner-image">
+                <img
+                  className="h-75"
+                  src="https://creavast.com/wp-content/uploads/2021/01/hxZtEnhSdf-1.png"
+                  alt="Banner"
+                />
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </>
     );
 }
 
--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -1,111 +1,137 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
 
 const styleIn = {
-    borderRadius: "20px",
+  borderRadius: "20px",
 };
 const styleBtn = {
-    borderRadius: "5px",
+  borderRadius: "5px",
 };
 
 const fakeData = [
-    {
-        Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-7.jpg",
-    },
-    {
-        Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-9.jpg",
-    },
-    {
-        Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-11.jpg",
-    },
-    {
-        Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-13.jpg",
-    },
-    {
-        Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-15.jpg",
-    },
-    {
-        Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-17.jpg",
-    },
-    {
-        Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-19.jpg",
-    },
-    {
-        Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-21.jpg",
-    },
-    {
-        Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-23.jpg",
-    },
+  {
+    Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-7.jpg",
+  },
+  {
+    Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-9.jpg",
+  },
+  {
+    Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-11.jpg",
+  },
+  {
+    Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-13.jpg",
+  },
+  {
+    Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-15.jpg",
+  },
+  {
+    Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-17.jpg",
+  },
+  {
+    Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-19.jpg",
+  },
+  {
+    Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-21.jpg",
+  },
+  {
+    Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-23.jpg",
+  },
 ];
 
 const Service = () => {
-    return (
-        <Container>
-            <Row>
-                <div className="d-flex align-items-center justify-content-center mt-4">
-                    <h3>
-                        <strong>SERVICES</strong>
-                    </h3>
-                </div>
-                <div className="d-flex align-items-center justify-content-center">
-                    <h1>
-                        WHAT <span className="text-danger">WE DO</span>
-                    </h1>
-                </div>
-                <div className="d-flex align-items-center justify-content-center">
-                    <p>
-                        We are composers who appreciate words, motions, audio
-                        and graphics to come up with a <br />
-                        reader-friendly masterpiece that is appreciated by the
-                        local customer you are targeting.
-                    </p>
-                </div>
-            </Row>
-            <Row xs={1} md={3} className="g-4">
-                {fakeData.map((data) => (
-                    <Col>
-                        <Card className=" border-0">
-                            <Card.Body>
-                                <img src={data.Image} alt="" />
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
-            <div>
-                <div
-                    style={styleIn}
-                    className=" bg-info text-dark align-align-items-center justify-content-center "
-                >
-                    <div className="d-flex align-items-center justify-content-center mt-5">
-                        <h2 className="mt-5">GET UPDATE REGULARLY</h2>
-                    </div>
-                    <div className="d-flex align-items-center justify-content-center">
-                        <p>
-                            We won't publish your email with 3rd parties. Trust
-                            us and get regular updates
-                        </p>
-                    </div>
-                    <div className="d-flex align-items-center justify-content-center ">
-                        <input
-                            className="mb-5"
-                            type="email"
-                            name="email"
-                            placeholder="Your Email Address"
-                            id=""
-                        />
-                        <button
-                            style={styleBtn}
-                            className="text-dark bg-warning mb-5 "
-                            type="button"
-                        >
-                            SUBMIT
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </Container>
-    );
+  return (
+    <Container>
+      <Row>
+        <div className="d-flex align-items-center justify-content-center mt-4">
+          <h3>
+            <strong>SERVICES</strong>
+          </h3>
+        </div>
+        <div className="d-flex align-items-center justify-content-center">
+          <h1>
+            WHAT <span className="text-danger">WE DO</span>
+          </h1>
+        </div>
+        <div className="d-flex align-items-center justify-content-center">
+          <p>
+            We are composers who appreciate words, motions, audio and graphics
+            to come up with a <br />
+            reader-friendly masterpiece that is appreciated by the local
+            customer you are targeting.
+          </p>
+        </div>
+      </Row>
+      <Row xs={1} md={3} className="g-2">
+        {fakeData.map((data) => (
+          <SingleCard data={data} />
+        ))}
+      </Row>
+      <div>
+        <div
+          style={styleIn}
+          className=" bg-info text-dark align-align-items-center justify-content-center "
+        >
+          <div className="d-flex align-items-center justify-content-center mt-5">
+            <h2 className="mt-5">GET UPDATE REGULARLY</h2>
+          </div>
+          <div className="d-flex align-items-center justify-content-center">
+            <p>
+              We won't publish your email with 3rd parties. Trust us and get
+              regular updates
+            </p>
+          </div>
+          <div className="d-flex align-items-center justify-content-center ">
+            <input
+              className="mb-5"
+              type="email"
+              name="email"
+              placeholder="Your Email Address"
+              id=""
+            />
+            <button
+              style={styleBtn}
+              className="text-dark bg-warning mb-5 "
+              type="button"
+            >
+              SUBMIT
+            </button>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Service;
+
+const SingleCard = ({ data }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <Col>
+      <Card
+        className=" border-0 p-0 "
+        style={{
+          height: "300px",
+          width: "300px",
+          backgroundColor: isHovered ? "yellow" : "",
+        }}
+      >
+        <Card.Body
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          {isHovered ? (
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam
+              nulla reiciendis tenetur assumenda laboriosam, exercitationem
+              minima praesentium recusandae in magnam necessitatibus quos rerum
+              omnis aspernatur nihil maiores doloremque eius quo!
+            </p>
+          ) : (
+            <img src={data.Image} alt="" width="250px" />
+          )}
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
