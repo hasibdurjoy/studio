b96bf7b78b03d01b73165b70f5d07ee634a42d6c
arindom10 <karindom7@gmail.com>
2023-08-29 19:16:57 +0600

    service updated

--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
 
+const styleIn = {
+    borderRadius: "20px",
+};
+const styleBtn = {
+    borderRadius: "5px",
+};
+
 const fakeData = [
     {
         Image: "https://creavast.com/wp-content/uploads/2021/05/1-36-7.jpg",
@@ -65,12 +72,37 @@ const Service = () => {
                     </Col>
                 ))}
             </Row>
-            <div className="d-flex align-align-items-center justify-content-center">
-                <img
-                    height="300px"
-                    src="https://creavast.com/wp-content/uploads/2021/01/hero-bg-2.jpg"
-                    alt=""
-                />
+            <div>
+                <div
+                    style={styleIn}
+                    className=" bg-info text-dark align-align-items-center justify-content-center "
+                >
+                    <div className="d-flex align-items-center justify-content-center mt-5">
+                        <h2 className="mt-5">GET UPDATE REGULARLY</h2>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-center">
+                        <p>
+                            We won't publish your email with 3rd parties. Trust
+                            us and get regular updates
+                        </p>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-center ">
+                        <input
+                            className="mb-5"
+                            type="email"
+                            name="email"
+                            placeholder="Your Email Address"
+                            id=""
+                        />
+                        <button
+                            style={styleBtn}
+                            className="text-dark bg-warning mb-5 "
+                            type="button"
+                        >
+                            SUBMIT
+                        </button>
+                    </div>
+                </div>
             </div>
         </Container>
     );
