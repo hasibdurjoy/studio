a58cab3b3ed775003bda698f52a6ceb378c8d699
arindom10 <karindom7@gmail.com>
2023-08-30 12:41:06 +0600

    Marketing services updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,64 +6,65 @@ const Header = () => {
     <div className="mx-5 pt-1">
       <div className="d-flex align-items-center justify-content-between">
         <div>
-          <img
-            src="https://i.ibb.co/smdhcQm/1.png"
-            alt=""
-            height="60px"
-          />
+          <img src="https://i.ibb.co/smdhcQm/1.png" alt="" height="60px" />
         </div>
         <div className="d-flex align-items-center  gap-5">
-        <Nav className="me-auto">
+          <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Join Us</Nav.Link>
             <NavDropdown title="Who We Are" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">About Us</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Clients
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Clients</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Awards</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                CRS
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">CRS</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Services" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Brand Strategy </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-              Creative Services
+              <NavDropdown.Item href="#action/3.1">
+                Brand Strategy{" "}
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Social Listening & ORM</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Creative Services
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                Social Listening & ORM
+              </NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
-              Media Planning & Buying
+                Media Planning & Buying
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.5">
-              Marketing Automation
+                Marketing Automation
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.6">
-              Technology Builds
+                Technology Builds
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.7">
-              Healthcare Marketing
+                Healthcare Marketing
               </NavDropdown.Item>
             </NavDropdown>
             <Nav.Link href="#link">Say Hello</Nav.Link>
             <NavDropdown title="News and Insights" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">
-                            Insights
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Insights</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-              Thought Leadership
+                Thought Leadership
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3"> Media Coverage</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                {" "}
+                Media Coverage
+              </NavDropdown.Item>
               <NavDropdown.Divider />
             </NavDropdown>
             <NavDropdown title="Resources" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Mirumark Events</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-              Creative Showcase
+              <NavDropdown.Item href="#action/3.1">
+                Mirumark Events
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">MarTech Report</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Creative Showcase
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                MarTech Report
+              </NavDropdown.Item>
               <NavDropdown.Divider />
             </NavDropdown>
           </Nav>
@@ -74,9 +75,3 @@ const Header = () => {
 };
 
 export default Header;
-
-
-
-
-
-
--- src/Components/MarketingServices.jsx
+++ src/Components/MarketingServices.jsx
@@ -12,61 +12,57 @@ const MarketingServices = () => {
         "https://www.mirumindia.com/hubfs/communication-brand-start-no-text.png",
     },
     {
-      id: 1,
-      name: "Brand Strategy",
+      id: 2,
+      name: "Creative Services",
       details:
-        "We firmly believe in our ‘digital first’ credence; we are not an ‘only digital’ agency.",
-      image:
-        "https://www.mirumindia.com/hubfs/communication-brand-start-no-text.png",
+        "Long & short-form content, videos, blogs, white papers, and case studies, we have you covered.",
+      image: "https://www.mirumindia.com/hubfs/creative-service-no-text-.png",
     },
     {
-      id: 1,
-      name: "Brand Strategy",
+      id: 3,
+      name: "Media buying & planning",
       details:
-        "We firmly believe in our ‘digital first’ credence; we are not an ‘only digital’ agency.",
+        "Strategy, influencer, programmatic, and placement of advertisements are all part of media planning.",
       image:
-        "https://www.mirumindia.com/hubfs/communication-brand-start-no-text.png",
+        "https://www.mirumindia.com/hubfs/media-planning-and-buying-no-text.png",
     },
     {
-      id: 1,
-      name: "Brand Strategy",
+      id: 4,
+      name: "Social Listening",
       details:
-        "We firmly believe in our ‘digital first’ credence; we are not an ‘only digital’ agency.",
-      image:
-        "https://www.mirumindia.com/hubfs/communication-brand-start-no-text.png",
+        "Mirumark has built extensive domain expertise in Social Listening over the last 11 years. Our 24*7 practice.",
+      image: "https://www.mirumindia.com/hubfs/social-listening-no-text.png",
     },
     {
-      id: 1,
-      name: "Brand Strategy",
+      id: 5,
+      name: "MarTech & Marketing Automation",
       details:
-        "We firmly believe in our ‘digital first’ credence; we are not an ‘only digital’ agency.",
+        "Mirumark is a Salesforce Crest Consulting Partner with 10+ years of experience in providing Marketing Automation services.",
       image:
-        "https://www.mirumindia.com/hubfs/communication-brand-start-no-text.png",
+        "https://www.mirumindia.com/hubfs/marketing-automation-no-text.png",
     },
     {
-      id: 1,
-      name: "Brand Strategy",
+      id: 6,
+      name: "Technology Solutions",
       details:
-        "We firmly believe in our ‘digital first’ credence; we are not an ‘only digital’ agency.",
-      image:
-        "https://www.mirumindia.com/hubfs/communication-brand-start-no-text.png",
+        "Mirumark understands your brand needs and provides all the technology requirements – websites, mobile applications, chatbots, SEO, and Sitecore – under one roof.",
+      image: "https://www.mirumindia.com/hubfs/technology-build-no-text.png",
     },
     {
-      id: 1,
-      name: "Brand Strategy",
+      id: 7,
+      name: "Healthcare marketing",
       details:
-        "We firmly believe in our ‘digital first’ credence; we are not an ‘only digital’ agency.",
-      image:
-        "https://www.mirumindia.com/hubfs/communication-brand-start-no-text.png",
+        "Mirumark Health – is a specialty brand – that we have created to deliver end-to-end, digital-first, healthcare communication services.",
+      image: "https://www.mirumindia.com/hubfs/mirum-health-no-text.png",
     },
   ];
   return (
     <Container className="py-5 d-flex flex-column gap-3">
       <h2 className="fs-1 text-center">Our Digital Marketing Services</h2>
       <p>
-        Mirumark India is an award-winning vibrant and dynamic full-service Digital
-        Marketing Agency that has been in business for about 12 years. To
-        provide our digital marketing partners with top-notch services, we
+        Mirumark India is an award-winning vibrant and dynamic full-service
+        Digital Marketing Agency that has been in business for about 12 years.
+        To provide our digital marketing partner s with top-notch services, we
         continue to enhance our processes and invest in cutting-edge tools and
         resources as we expand.
       </p>
