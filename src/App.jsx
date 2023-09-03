1a2c386d1d27c460179db454a793bf56a64a7bb6
hasibdurjoy <hasibdurjoy5@gmail.com>
2023-09-03 11:47:44 +0600

    layout change and remove unncessary files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import HomePage from "./Pages/HomePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
--- src/Components/HomePageOverview.jsx
+++ src/Components/HomePageOverview.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import Banner from "./Banner";
-import Header from "./Header";
-import Footer from "./Footer";
 
 const HomePageOverview = () => {
   return (
     <div>
-      <Header />
       <Banner />
-      <Footer />
     </div>
   );
 };
