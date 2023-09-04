58b17d5fb09adb5db286fd55cd2d6b97fd7a1105
hasibdurjoy <hasibdurjoy5@gmail.com>
2023-09-04 23:51:50 +0600

    service

--- src/Components/HomePageOverview.jsx
+++ src/Components/HomePageOverview.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "./Banner";
 import About from "./About";
 import Experiences from "./Experiences";
+import Services from "./Services";
 
 const HomePageOverview = () => {
   return (
@@ -9,6 +10,7 @@ const HomePageOverview = () => {
       <Banner />
       <About />
       <Experiences />
+      <Services />
     </div>
   );
 };
---
+++ src/Components/Services.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+const Services = () => {
+  return (
+    <Container className="mt-5">
+      <h4 className="text-center">Our Services</h4>
+      <h2 className="text-center">How We Can Help?</h2>
+      <Row className="mt-5">
+        <Col md={4}>
+          <div className="d-flex align-items-start mb-2">
+            <div>
+              <h2 className="text-end">HSHSHS</h2>
+              <p className="text-end">
+                Helping the business to steal the spotlight with higher
+                conversions and traffic
+              </p>
+            </div>
+            <div>
+              <img
+                src="https://www.hitsvalue.com/wp-content/uploads/2019/12/home4_icon1-168x168.png"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="d-flex align-items-start mb-2">
+            <div>
+              <h2 className="text-end">HSHSHS</h2>
+              <p className="text-end">
+                Helping the business to steal the spotlight with higher
+                conversions and traffic
+              </p>
+            </div>
+            <div>
+              <img
+                src="https://www.hitsvalue.com/wp-content/uploads/2019/12/home4_icon1-168x168.png"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="d-flex align-items-start mb-2">
+            <div>
+              <h2 className="text-end">HSHSHS</h2>
+              <p className="text-end">
+                Helping the business to steal the spotlight with higher
+                conversions and traffic
+              </p>
+            </div>
+            <div>
+              <img
+                src="https://www.hitsvalue.com/wp-content/uploads/2019/12/home4_icon1-168x168.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </Col>
+        <Col md={4}>
+          <div className="d-flex justify-content-center">
+            <img
+              src="https://www.hitsvalue.com/wp-content/uploads/2019/12/man_with_laptop.png"
+              alt=""
+            />
+          </div>
+        </Col>
+        <Col md={4}>
+          <div className="d-flex align-items-start mb-2">
+            <div>
+              <img
+                src="https://www.hitsvalue.com/wp-content/uploads/2019/12/home4_icon1-168x168.png"
+                alt=""
+              />
+            </div>
+            <div>
+              <h2 className="text-start">HSHSHS</h2>
+              <p className="text-start">
+                Helping the business to steal the spotlight with higher
+                conversions and traffic
+              </p>
+            </div>
+          </div>
+          <div className="d-flex align-items-start mb-2">
+            <div>
+              <img
+                src="https://www.hitsvalue.com/wp-content/uploads/2019/12/home4_icon1-168x168.png"
+                alt=""
+              />
+            </div>
+            <div>
+              <h2 className="text-start">HSHSHS</h2>
+              <p className="text-start">
+                Helping the business to steal the spotlight with higher
+                conversions and traffic
+              </p>
+            </div>
+          </div>
+          <div className="d-flex align-items-start mb-2">
+            <div>
+              <img
+                src="https://www.hitsvalue.com/wp-content/uploads/2019/12/home4_icon1-168x168.png"
+                alt=""
+              />
+            </div>
+            <div>
+              <h2 className="text-start">HSHSHS</h2>
+              <p className="text-start">
+                Helping the business to steal the spotlight with higher
+                conversions and traffic
+              </p>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Services;
